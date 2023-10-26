8b1096ce0ebb0ecf0b69cc7c22e638cc7c000395
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-26 05:41:44 +0000

    update deno.lock

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   test,
   build,
-} from "https://pkg.fluentci.io/bazel_pipeline@v0.3.0/mod.ts";
+} from "https://pkg.fluentci.io/bazel_pipeline@v0.3.1/mod.ts";
 
 await test();
 await build();
