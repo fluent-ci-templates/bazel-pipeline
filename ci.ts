2eeb4ef733d8ec472af25be0d9f6e73a4e43d2e6
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-08 04:46:19 +0000

    feat: handle pipeline arguments

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   test,
   build,
-} from "https://pkg.fluentci.io/bazel_pipeline@v0.3.1/mod.ts";
+} from "https://pkg.fluentci.io/bazel_pipeline@v0.4.0/mod.ts";
 
 await test();
 await build();
