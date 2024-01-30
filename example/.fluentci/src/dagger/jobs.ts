23f92b05e15c3de6c023ac20575bae21debd593f
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-30 09:03:54 +0000

    fix issue with dagger connect
    update README.md

--- example/.fluentci/src/dagger/jobs.ts
+++ example/.fluentci/src/dagger/jobs.ts
@@ -1,5 +1,4 @@
-import Client, { Directory } from "../../deps.ts";
-import { connect } from "../../sdk/connect.ts";
+import { Directory, dag } from "../../deps.ts";
 import { getDirectory } from "./lib.ts";
 
 export enum Job {
@@ -28,32 +27,29 @@ export async function build(
   src: Directory | string,
   version?: string
 ): Promise<Directory | string> {
-  let id = "";
-  await connect(async (client: Client) => {
-    const BAZEL_VERSION = Deno.env.get("BAZEL_VERSION") || version || "6.3.2";
-    const context = getDirectory(client, src);
-    const ctr = client
-      .pipeline(Job.build)
-      .container()
-      .from("ghcr.io/fluentci-io/bazel:latest")
-      .withMountedCache("/root/.cache/bazel", client.cacheVolume("bazel-cache"))
-      .withEnvVariable("BAZEL_VERSION", BAZEL_VERSION)
-      .withDirectory("/app", context, {
-        exclude,
-      })
-      .withWorkdir("/app")
-      .withExec(["bazelisk", "build", "//..."])
-      .withExec(["ls", "-la"])
-      .withExec(["sh", "-c", "mkdir -p output && cp -r bazel-bin/* output"]);
+  const BAZEL_VERSION = Deno.env.get("BAZEL_VERSION") || version || "6.3.2";
+  const context = await getDirectory(dag, src);
+  const ctr = dag
+    .pipeline(Job.build)
+    .container()
+    .from("ghcr.io/fluentci-io/bazel:latest")
+    .withMountedCache("/root/.cache/bazel", dag.cacheVolume("bazel-cache"))
+    .withEnvVariable("BAZEL_VERSION", BAZEL_VERSION)
+    .withDirectory("/app", context, {
+      exclude,
+    })
+    .withWorkdir("/app")
+    .withExec(["bazelisk", "build", "//..."])
+    .withExec(["ls", "-la"])
+    .withExec(["sh", "-c", "mkdir -p output && cp -r bazel-bin/* output"]);
 
-    const result = await ctr.stdout();
+  const result = await ctr.stdout();
 
-    console.log(result);
+  console.log(result);
 
-    const output = ctr.directory("/app/output");
-    await output.export("output");
-    id = await output.id();
-  });
+  const output = ctr.directory("/app/output");
+  await output.export("output");
+  const id = await output.id();
   return id;
 }
 
@@ -68,25 +64,21 @@ export async function test(
   src: Directory | string,
   version?: string
 ): Promise<string> {
-  await connect(async (client: Client) => {
-    const BAZEL_VERSION = Deno.env.get("BAZEL_VERSION") || version || "6.3.2";
-    const context = getDirectory(client, src);
-    const ctr = client
-      .pipeline(Job.test)
-      .container()
-      .from("ghcr.io/fluentci-io/bazel:latest")
-      .withMountedCache("/root/.cache/bazel", client.cacheVolume("bazel-cache"))
-      .withEnvVariable("BAZEL_VERSION", BAZEL_VERSION)
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withExec(["bazelisk", "test", "//..."])
-      .withExec(["ls", "-la"]);
+  const BAZEL_VERSION = Deno.env.get("BAZEL_VERSION") || version || "6.3.2";
+  const context = await getDirectory(dag, src);
+  const ctr = dag
+    .pipeline(Job.test)
+    .container()
+    .from("ghcr.io/fluentci-io/bazel:latest")
+    .withMountedCache("/root/.cache/bazel", dag.cacheVolume("bazel-cache"))
+    .withEnvVariable("BAZEL_VERSION", BAZEL_VERSION)
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["bazelisk", "test", "//..."])
+    .withExec(["ls", "-la"]);
 
-    const result = await ctr.stdout();
-
-    console.log(result);
-  });
-  return "Done";
+  const result = await ctr.stdout();
+  return result;
 }
 
 export type JobExec = (
